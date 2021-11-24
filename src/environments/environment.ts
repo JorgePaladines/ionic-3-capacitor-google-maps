7c785441b516d3847a8c34955ac014a577d97b63
Jorge Paladines <jorge.paladines.bossano@outlook.com>
2021-11-24 08:48:56 -0500

    Inicial

---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: false,
+    mapsKey: 'AIzaSyBX5mqL0FHq0fT9FxnkRTbSSqGPgLWaCOM'
+  };
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { CapacitorGoogleMaps } from '@capacitor-community/capacitor-googlemaps-native';
+import { Geolocation } from '@capacitor/geolocation';
+import { environment } from '../../environments/environment';
 
 @Component({
   selector: 'page-home',
@@ -7,7 +9,55 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  @ViewChild('map') mapView: ElementRef;
+
+  constructor() {
+    CapacitorGoogleMaps.initialize({
+      key: environment.mapsKey
+    });
+  }
+
+  ionViewDidEnter() {
+    this.loadMap('normal');
+  }
+
+  ionViewDidLeave() {
+    CapacitorGoogleMaps.close();
+  }
+
+  loadMap(type_map) {
+    Geolocation.requestPermissions().then(async premission => {
+      const coordinates = await Geolocation.getCurrentPosition();
+
+      console.log(coordinates, "coordinates");
+
+      const boundingRect = this.mapView.nativeElement.getBoundingClientRect() as DOMRect;
+
+      CapacitorGoogleMaps.create({
+          width: Math.round(boundingRect.width),
+          height: Math.round(boundingRect.height),
+          x: Math.round(boundingRect.x),
+          y: Math.round(boundingRect.y),
+          latitude: coordinates.coords.latitude,
+          longitude: coordinates.coords.longitude,
+          zoom: 16
+      });
+
+      CapacitorGoogleMaps.addListener('onMapReady', async () => {
+        console.log("MAP READY");
+
+        CapacitorGoogleMaps.setMapType({
+            type: type_map
+        });
+
+        // Create our current location marker
+        CapacitorGoogleMaps.addMarker({
+            latitude: coordinates.coords.latitude,
+            longitude: coordinates.coords.longitude,
+            title: 'Mi ubicación',
+        });
+      });
+    });
 
   }
 
